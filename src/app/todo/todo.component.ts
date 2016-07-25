f5fd03fe737711eda13adfd83ba50dcf2d98eb41
Dave Spring <davespring.79@gmail.com>
2016-07-25 11:58:00 -0400

    CompletedTodo

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -23,7 +23,8 @@ export class TodoComponent implements OnInit {
     console.log("TodoComponent Initialized")
   }
 
-  
+  // This would go in the todo.service
+  // and we could then change the addTodoFinal to addTodo
   addTodo(todo: Todo){
     if (!todo.id) {
       todo.id = ++this.lastId;
@@ -47,6 +48,42 @@ export class TodoComponent implements OnInit {
 
     if(this.todos.length == 0){
       this.lastId = 0;
+    }
+  }
+
+
+
+  getTodoById(id: number): Todo {
+    return this.todos
+      .filter(todo => todo.id === id)
+      .pop();
+  }
+
+  updateTodoById(id: number, values: Object = {}): Todo {
+    let todo = this.getTodoById(id);
+    if (!todo) {
+      return null;
+    }
+    Object.assign(todo, values);
+    return todo;
+  }
+
+  // Toggle todo complete
+  toggleTodoComplete(todo: Todo){
+    let updatedTodo = this.updateTodoById(todo.id, {
+      complete: !todo.complete
+    });
+
+    return updatedTodo;
+  }
+
+
+  // Update todo item text-decoration
+  strikeThrough(todo: Todo){
+    if(todo.complete){
+      return "line-through";
+    }else{
+      return "";
     }
   }
 
